ef2f6812a810ac6e01291ccf686264bf628303e0
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-03-09 23:11:06 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@keyframes rotate360 {
+  to { transform: rotate(360deg); }
+}
+img { animation: 2s rotate360 infinite linear; }
+</style>
